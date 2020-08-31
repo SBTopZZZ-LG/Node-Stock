41b5a4edb15f6846300e981b97cd3c6bd2db7190
Saumitra Topinkatti <sbtopinkatti@gmail.com>
2020-08-31 14:02:05 +0530

    Stable working

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 //Stock Market portfolio app by SBTOPZZZ
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs  = require('express-handlebars');
-
+const request = require('request');
+const body_parser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+
+// Use body parser middleware
+app.use(body_parser.urlencoded({extended: false}));
+
+// API key = pk_30f57c6112ce4846a80c05c95fffe2d2
+// Create call API function
+function call_api(finished_api, ticker) {
+	var ticker2 = "fb";
+	if (ticker) ticker2=ticker;
+	request("https://cloud.iexapis.com/stable/stock/"+ticker2+"/quote?token=pk_30f57c6112ce4846a80c05c95fffe2d2", {json: true}, (err, res, body) => {
+	if (err) return console.log(err);
+	finished_api(body);
+});
+};
 
 // Set handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-const otherstuff = "Hello there! This is other stuff!"
+const otherstuff = "Hello there! This is other stuff!";
 
-// Set handlebar routes
+// Set handlebar index GET route
 app.get('/', function (req, res) {
-    res.render('home', {
-    	stuff: otherstuff
-    });
+	call_api(function(doneAPI) {
+		res.render('home', {
+    		stock: doneAPI
+    	});
+	});
+});
+
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+	posted_stuff = req.body.stock_ticker;
+	call_api(function(doneAPI) {
+		res.render('home', {
+    		stock: doneAPI
+    	});
+	}, posted_stuff);
+});
+
+// Create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set static folder
